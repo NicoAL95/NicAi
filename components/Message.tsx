7ce982ef0cc4921250f88bb9c0c4572be49525af
NicoAL95 <nicoabellaia@gmail.com>
2023-02-28 12:01:27 +0700

    Update img bug

--- components/Message.tsx
+++ components/Message.tsx
@@ -11,7 +11,7 @@ function Message({
   return (
     <div className={`py-5 text-white ${isNicAi && "bg-[#434654]"}`}>
         <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-            <img src="message.user.avatar" alt="" className="h-8 w-8" />
+            <img src={message.user.avatar} alt="" className="h-8 w-8" />
             <p>
                 {message.text}
             </p>
